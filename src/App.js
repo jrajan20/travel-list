9cd2ad44ea10c92c6cd5c64ff5ef600aa168fcc1
jrajan20 <jai_20j@hotmail.com>
2026-06-16 19:38:55 -0400

    Populating list items

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true }
 ];
 
 export default function App(){
@@ -28,18 +29,26 @@ export default function App(){
   }
 
   function PackingList(){
-    return <ul className="list">
-      {initialItems.map(item => 
+    return (
+    <div className="list">
+      <ul>
+        {initialItems.map(item => 
 
-        <item item={item}/>
+          <Item item={item}/>
 
-      )}
-      </ul>
-
+        )}
+        </ul>
+    </div>
+    )
   }
 
   function Item({item}){
-    <div>Test</div>
+    return (
+    <li>
+      <span style={item.packed ?{textDecoration: "line-through"}:{}}>{item.quantity} {item.description}</span>
+     <button>❌</button>
+    </li>
+    )
   }
   function Stats(){
     return(
