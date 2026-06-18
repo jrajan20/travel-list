cdb11728ee5b385cd22da37f71253d5c1279fc3d
jrajan20 <jai_20j@hotmail.com>
2026-06-18 00:05:43 -0400

    Added sorting by input order, description, and packed status

--- src/App.js
+++ src/App.js
@@ -81,15 +81,33 @@ function handleToggleItem(id){
   }
 
   function PackingList({items, onDeleteItems, toggleItems}){
+
+    const [sortBy,setSortBy] = useState("input");
+    let sortedItems;
+
+    if(sortBy === "input") sortedItems = items;
+    if(sortBy === "description"){
+      sortedItems = items.slice().sort((a,b) => a.description.localeCompare(b.description));
+    }
+    if(sortBy === "packed"){
+      sortedItems = items.slice().sort((a,b) => Number(a.packed) - Number(b.packed));
+    }
     return (
     <div className="list">
       <ul>
-        {items.map(item => 
+        {sortedItems.map(item => 
 
           <Item item={item} items={items} deleteItems={onDeleteItems} toggleItems={toggleItems} key={item.id}/>
 
         )}
         </ul>
+        <div className="actions">
+          <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+            <option value='input'> Sort by input order</option>
+            <option value='description'>Sort by description</option>
+            <option value='packed'>Sort by packed</option>
+          </select>
+        </div>
     </div>
     )
   }
